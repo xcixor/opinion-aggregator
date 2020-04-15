2ffe30931cc84541122ed713cdefe2f2cb2505fd
xcixor <pndungu54@gmail.com>
2020-04-15 06:48:40 +0300

    chore(sections): display more sections

--- src/core/opinion_aggregator/templatetags/section_extras.py
+++ src/core/opinion_aggregator/templatetags/section_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count, Q
 from opinion_aggregator.models import survey
 
 register = template.Library()
@@ -13,3 +14,29 @@ def get_count(response):
 def get_unique(question):
     questions = survey.SurveyResponsesModel.objects.get(question=question).first()
     return questions
+
+@register.filter
+def get_leadership_yes_responses(question):
+    responses = survey.SurveyResponsesModel.objects.filter(question=question, response="Yes")
+    return len(responses)
+
+@register.filter
+def get_leadership_no_responses(question):
+    responses = survey.SurveyResponsesModel.objects.filter(question=question, response="No")
+    return len(responses)
+
+
+@register.filter
+def get_salary_count(question):
+    responses = survey.SurveyResponsesModel.objects.filter(question=question, response="have a job with salary")
+    return len(responses)
+
+@register.filter
+def get_entreprenuer_count(question):
+    responses = survey.SurveyResponsesModel.objects.filter(question=question, response="being a entrepreneur and have a you own businesses")
+    return len(responses)
+
+@register.filter
+def get_most_popular_industry(industry):
+    response = survey.SurveyResponsesModel.objects.filter(response=industry)
+    return len(response)
