9b2d58f5b7e25407cc348552351f26d8a62b54f9
xcixor <pndungu54@gmail.com>
2020-04-15 08:30:51 +0300

    add fixes

--- src/core/opinion_aggregator/admin.py
+++ src/core/opinion_aggregator/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from opinion_aggregator.models import User
 from opinion_aggregator.models import survey
 
+class ResponsesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'response')
+
 admin.site.register(User)
 admin.site.register(survey.OptionModel)
 admin.site.register(survey.QuestionModel)
 admin.site.register(survey.PartModel)
 admin.site.register(survey.SectionModel)
 admin.site.register(survey.SurveyModel)
-admin.site.register(survey.SurveyResponsesModel)
+admin.site.register(survey.SurveyResponsesModel, ResponsesAdmin)
 admin.site.register(survey.QuestionOptions)
 # Register your models here.
