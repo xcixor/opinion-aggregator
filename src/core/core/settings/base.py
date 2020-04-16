23c3d92b99535605e22eacec978c48c49c90aa1b
xcixor <pndungu54@gmail.com>
2020-04-17 01:50:23 +0300

    chore(db): remove sqlite

--- src/core/core/settings/base.py
+++ src/core/core/settings/base.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_IP'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
