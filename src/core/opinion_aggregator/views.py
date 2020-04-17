598b08688326d2df08c3f3c2bc03eb8b7b7683bd
xcixor <pndungu54@gmail.com>
2020-04-17 07:21:37 +0300

    add user content

--- src/core/opinion_aggregator/views.py
+++ src/core/opinion_aggregator/views.py
@@ -51,6 +51,7 @@ def authenticate_user(request):
             if next_page:
                 message = "Welcome {}!".format(user.email)
                 messages.success(request, message, extra_tags='green')
+                print(next_page, '*******************')
                 return redirect(next_page)
             message = "Welcome {}!".format(user.email)
             messages.success(request, message)
